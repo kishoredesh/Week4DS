238adebf1c7cb0c28e50ff7044fa4b2e4312e837
kishoredesh <kishored@universal-sw.com>
2016-10-28 15:58:21 -0400

    Getting ready for assignment

---
+++ factorial.r
@@ -0,0 +1,10 @@
+factorial <- function(num)
+  
+{
+  if  (num == 1)
+  { return(num)
+}
+else {return(num*factorial(num-1) )}
+}
+ 
+            
---
+++ myfactorial.r
@@ -0,0 +1,10 @@
+myfactorial <- function(num)
+  
+{
+  if  (num == 1)
+  { return(num)
+}
+else {return(num*myfactorial(num-1) )}
+}
+ 
+            
